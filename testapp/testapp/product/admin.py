2884c7cfb3405d9722c73b5720d8350f3c39dc51
Code FL <codefl007@gmail.com>
2020-06-06 14:16:14 -0700

    Added comment to tag admin UI

--- testapp/testapp/product/admin.py
+++ testapp/testapp/product/admin.py
@@ -2,15 +2,27 @@ from django.contrib import admin
 from .models import ProductModel, ProductVariationModel, CategoryModel, TagModel
 
 
+#########################################################
+# Many to many inline edit UI
+#########################################################
+
 class ProductTagInline(admin.TabularInline):
     model = ProductModel.tags.through
     extra = 1
 
 
+#########################################################
+# 1 to many inline edit UI
+#########################################################
+
 class ProductVariationModelInline(admin.TabularInline):
     model = ProductVariationModel
     extra = 1
 
+
+#########################################################
+# Define admin model for product (main object)
+#########################################################
 
 class ProductModelAdmin(admin.ModelAdmin):
     fields = ['name', 'description', 'category']
@@ -19,6 +31,10 @@ class ProductModelAdmin(admin.ModelAdmin):
 
     inlines = [ProductVariationModelInline, ProductTagInline]
 
+
+#########################################################
+# Register the 1st level admin models
+#########################################################
 
 admin.site.register(ProductModel, ProductModelAdmin)
 admin.site.register(CategoryModel)
